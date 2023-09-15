6d44dc779d03652b668fd285b688755df2d03d61
Christian Nagel <christian@christiannagel.com>
2023-09-15 21:34:27 +0200

    interceptors sample

---
+++ csharp/Interceptors/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Interceptors;
+
+Console.WriteLine("Hello, World!");
+
+Runner r = new();
+r.Bar();
---
+++ csharp/Interceptors/Runner.cs
@@ -0,0 +1,15 @@
+﻿namespace Interceptors;
+
+public class Runner
+{
+    public void Bar()
+    {
+        DotheMagic();
+        //Foo();
+    }
+
+    public static void DotheMagic()
+    {
+        Console.WriteLine("Foo");
+    }
+}
