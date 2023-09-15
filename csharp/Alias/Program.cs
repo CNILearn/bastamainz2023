4c6c9b372b1eed376b4a94bfc8361fe87af8042f
Christian Nagel <christian@christiannagel.com>
2023-09-15 21:48:25 +0200

    alias any type

---
+++ csharp/Alias/Program.cs
@@ -0,0 +1,25 @@
+﻿using GameStart = (string Name, string GameType);
+using OneS = One<string>;
+using OneI = One<int>;
+
+GameStart s = new("Christian", "Game6x4");
+
+(string name, _) = s;
+
+Console.WriteLine(name);
+Console.WriteLine(s.GameType);
+
+OneS os = new();
+os.Foo("a string");
+
+OneI oi = new();
+oi.Foo(42);
+
+public class One<T>
+{
+    public void Foo(T item)
+    {
+        Console.WriteLine(item);
+    }
+}
+
