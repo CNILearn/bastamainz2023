f66bd6432b6768e0a1eb0bb1c5a01e7011eb289f
Christian Nagel <christian@christiannagel.com>
2023-09-21 14:51:49 +0200

    regex benchmark

---
+++ sourcegenerators/usingsourcegenerator/RegularExpressionSample/GlobalSuppressions.cs
@@ -0,0 +1,11 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Design", "CA1050:Declare types in namespaces", Justification = "Just a small sample", Scope = "module")]
+[assembly: SuppressMessage("GeneratedRegex", "SYSLIB1045:Convert to 'GeneratedRegexAttribute'.", Justification = "Sample showing differences with generated regex", Scope = "member", Target = "~F:TestIt.s_abcOrDef")]
+[assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Benchmark requires instance members", Scope = "member", Target = "~M:TestIt.TestWithReflection")]
+[assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "Benchmark requires instance members", Scope = "member", Target = "~M:TestIt.TestWithGenerator")]
--- sourcegenerators/usingsourcegenerator/RegularExpressionSample/Program.cs
+++ sourcegenerators/usingsourcegenerator/RegularExpressionSample/Program.cs
@@ -1,21 +1,73 @@
 ﻿using System.Text.RegularExpressions;
 
-// Regex abcOrDev = new("abc|def", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
-IsMatch("abc");
-IsMatch("ABC");
-IsMatch("ghi");
-IsMatch("def");
+BenchmarkRunner.Run(typeof(TestIt));
 
-static void IsMatch(string text)
+// without benchmark
+//TestIt testIt = new();
+//testIt.TestWithReflection();
+//testIt.TestWithGenerator();
+
+//Console.WriteLine($"reg: {TestIt.s_matches_r} {TestIt.s_notMatches_r}");
+//Console.WriteLine($"sc: {TestIt.s_matches_s} {TestIt.s_notMatches_s}");
+
+public class TestIt
 {
-    if (MyRegex.AbcOrDefRegex().IsMatch(text))
+    internal static int s_matches_r = 0;
+    internal static int s_notMatches_r = 0;
+    internal static int s_matches_s = 0;
+    internal static int s_notMatches_s = 0;
+
+    [Benchmark]
+    public void TestWithGenerator()
     {
-        Console.WriteLine($"Match with {text}");
+        IsMatch("abc");
+        IsMatch("abc");
+        IsMatch("ABC");
+        IsMatch("ghi");
+        IsMatch("def");
     }
-    else
+
+    [Benchmark]
+    public void TestWithReflection()
     {
-        Console.WriteLine($"No match with {text}");
+        IsMatchReflection("abc");
+        IsMatchReflection("abc");
+        IsMatchReflection("ABC");
+        IsMatchReflection("ghi");
+        IsMatchReflection("def");
+    }
+
+    static readonly Regex s_abcOrDef = new("abc|def", RegexOptions.IgnoreCase);
+
+    static void IsMatchReflection(string text)
+    {
+        if (s_abcOrDef.IsMatch(text))
+        {
+            Interlocked.Increment(ref s_matches_r);
+            // Console.WriteLine($"Match with {text}");
+        }
+        else
+        {
+            Interlocked.Increment(ref s_notMatches_r);
+            // Console.WriteLine($"No match with {text}");
+        }
+    }
+
+    static void IsMatch(string text)
+    {
+        if (MyRegex.AbcOrDefRegex().IsMatch(text))
+        {
+            Interlocked.Increment(ref s_matches_s);
+            // Console.WriteLine($"Match with {text}");
+        }
+        else
+        {
+            Interlocked.Increment(ref s_notMatches_s);
+            // Console.WriteLine($"No match with {text}");
+        }
     }
 }
 
