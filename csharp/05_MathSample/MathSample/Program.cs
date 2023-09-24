f4c78af0049a4fda01d280f912951972eb92f5ab
Christian Nagel <christian@christiannagel.com>
2023-09-24 14:58:09 +0200

    math sample

--- csharp/05_MathSample/MathSample/Program.cs
+++ csharp/05_MathSample/MathSample/Program.cs
@@ -1,16 +1,16 @@
 ﻿using System.Numerics;
 
 Point<int> pt1 = new(4, 5);
-Translation<int> tr1 = new(2, 3);
+Transformation<int> tr1 = new(2, 3);
 Point<int> pt2 = pt1 + tr1;  // move the point by the translation
 Console.WriteLine($"original: {pt1}; moved: {pt2}");
 
 Point<double> pt3 = new(X: 4.5, Y: 3.3);
-Translation<double> tr2 = new(XOffset: 2.2, YOffset: 3.3); 
+Transformation<double> tr2 = new(XOffset: 2.2, YOffset: 3.3); 
 Point<double> pt4 = pt3 + tr2;  // move the point by the translation
 Console.WriteLine($"original: {pt3}; moved: {pt4}");
 
-public record struct Translation<T>(T XOffset, T YOffset) 
+public record struct Transformation<T>(T XOffset, T YOffset) 
     where T : IAdditionOperators<T, T, T>
 {
 }
@@ -18,6 +18,6 @@ public record struct Translation<T>(T XOffset, T YOffset)
 public record struct Point<T>(T X, T Y) 
     where T : IAdditionOperators<T, T, T>
 {
-    public static Point<T> operator +(Point<T> left, Translation<T> right) =>
+    public static Point<T> operator +(Point<T> left, Transformation<T> right) =>
         left with { X = left.X + right.XOffset, Y = left.Y + right.YOffset };
 }
