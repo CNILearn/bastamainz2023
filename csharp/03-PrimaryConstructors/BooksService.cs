b4e4816ee306ae1f824eca5490aae94af7922b60
Christian Nagel <christian@christiannagel.com>
2023-09-24 14:00:48 +0200

    primary constructor

---
+++ csharp/03-PrimaryConstructors/BooksService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace PrimaryConstructors;
+public class BooksService(IBooksRepository booksRepository, ILogger<BooksService> logger)
+{
+    public async Task<IEnumerable<Book>> GetBooksAsync()
+    {
+        logger.LogInformation("Getting books");
+        return await booksRepository.GetBooksAsync();
+    }
+}
+
+
+public interface IBooksRepository
+{
+    Task<IEnumerable<Book>> GetBooksAsync();
+}
---
+++ csharp/03-PrimaryConstructors/Program.cs
@@ -0,0 +1,26 @@
+﻿Point p1 = new Point(3, 4);
+Point2 p2 = new Point2(3, 4);
+
+MyStruct ms1 = new(1, 2, 3);
+ms1.Foo();
+
+
+// properties with get and init accessor
+public record class Book(string Title, string? Publisher, DateOnly ReleaseDate);
+
+// properties with get and set accessor
+public record struct Point(double X, double Y);
+
+// properties with get and init accessor
+public readonly record struct Point2(double X, double Y);
+
+// Primary constructor
+public struct MyStruct(int x, int y, int z)
+{
+    private int _y = y;
+    public int X { get; init; } = x;
+    public void Foo()
+    {
+        Console.WriteLine($"X: {X}, y: {_y}, z: {z}");
+    }
+}
