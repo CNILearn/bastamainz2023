128f99dc6806d1809a6e8c095ea039772525fc17
Christian Nagel <christian@christiannagel.com>
2023-09-24 12:50:56 +0200

    required modifier

---
+++ csharp/02-PreRequiredModifier/Positional.cs
@@ -0,0 +1,7 @@
+﻿namespace Positional;
+
+// Positional records
+public record class Person(string FirstName, string LastName, string? Middlename = default);
+
+public record class Student(string FirstName, string LastName, string Course, string? Middlename = default)
+    : Person(FirstName, LastName, Middlename);
---
+++ csharp/02-RequiredModifier/Nominal.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Nominal;
+
+public class Person
+{
+    public Person()
+    {
+    }
+
+    [SetsRequiredMembers]
+    public Person(string first, string last) =>
+        (FirstName, LastName) = (first, last);
+
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
+    public string? MiddleName { get; init; }
+}
